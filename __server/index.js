e64b7a6c1281fb2540876d7268318def247d16ad
divcraft <t.ogonowski@wp.pl>
2020-11-26 14:51:33 +0100

    change app.get from / to * in server

--- __server/index.js
+++ __server/index.js
@@ -28,11 +28,12 @@ passportConfig();
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build/index.html'));
-  });
+  // app.get('/', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, '../build/index.html'));
+  // });
   app.get('*', (req, res) => {
-    res.redirect('/');
+    res.sendFile(path.resolve(__dirname, '../build/index.html'));
+    // res.redirect('/');
   });
 }
 
