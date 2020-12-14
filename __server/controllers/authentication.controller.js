fff6bdb9cc5c26314813a11a729c3d6cc6305a0c
divcraft <t.ogonowski@wp.pl>
2020-12-14 10:14:03 +0100

    create authentication controller and move logic from authentication.router to it

---
+++ __server/controllers/authentication.controller.js
@@ -0,0 +1,53 @@
+const passport = require('passport');
+const Author = require('../mongoose/models/author.model');
+const sendError = require('../utils/sendError');
+const sendEmailWithPassword = require('../utils/sendEmailWithPassword');
+
+module.exports = {
+  logInUser: (req, res, next) => {
+    passport.authenticate('local', (err, authUser) => {
+      if (err) sendError(err, res);
+      if (!authUser) return res.status(401).send('Wrong username or password.');
+      req.logIn(authUser, (logErr) => {
+        if (logErr) sendError(logErr, res);
+        const { user } = req.cookies;
+        if (user) {
+          res.status(200).send(req.user);
+        } else {
+          const expiryDate = new Date();
+          expiryDate.setMonth(expiryDate.getMonth() + 1);
+          const cookieProps = {
+            secure: true,
+            sameSite: 'strict',
+            expires: expiryDate,
+          };
+          res
+            .status(200)
+            .cookie('user', req.user._id, cookieProps)
+            .send(req.user);
+        }
+      });
+    })(req, res, next);
+  },
+  recoverPassword: (req, res) => {
+    const { email } = req.body;
+    Author.findOne({ privateEmail: email }, (err, userByPrivateEmail) => {
+      if (err) sendError(err, res);
+      if (userByPrivateEmail) {
+        sendEmailWithPassword(userByPrivateEmail, email, res);
+      } else {
+        Author.findOne(
+          { divcraftEmail: email },
+          (secErr, userByDivcraftEmail) => {
+            if (secErr) sendError(secErr, res);
+            if (userByDivcraftEmail) {
+              sendEmailWithPassword(userByPrivateEmail, email, res);
+            } else {
+              res.status(401).send('Email is not found.');
+            }
+          }
+        );
+      }
+    });
+  },
+};
--- __server/routes/authentication.router.js
+++ __server/routes/authentication.router.js
@@ -1,56 +1,9 @@
-const passport = require('passport');
 const router = require('express').Router();
-const Author = require('../mongoose/models/author.model');
-const sendEmailWithPassword = require('../utils/sendEmailWithPassword');
-const sendError = require('../utils/sendError');
+const authenticationController = require('../controllers/authentication.controller');
 
-router.post('/login', (req, res, next) => {
-  passport.authenticate('local', (err, authUser) => {
-    if (err) sendError(err, res);
-    if (!authUser) return res.status(401).send('Wrong username or password.');
-    req.logIn(authUser, (logErr) => {
-      if (logErr) sendError(logErr, res);
-      const { user } = req.cookies;
-      if (user) {
-        res.status(200).send(req.user);
-      } else {
-        const expiryDate = new Date();
-        expiryDate.setMonth(expiryDate.getMonth() + 1);
-        const cookieProps = {
-          secure: true,
-          sameSite: 'strict',
-          expires: expiryDate,
-        };
-        res
-          .status(200)
-          .cookie('user', req.user._id, cookieProps)
-          .send(req.user);
-      }
-    });
-  })(req, res, next);
-});
+const { logInUser, recoverPassword } = authenticationController;
 
-router.post('/recover-password', (req, res) => {
-  const { email } = req.body;
-
-  Author.findOne({ privateEmail: email }, (err, userByPrivateEmail) => {
-    if (err) sendError(err, res);
-    if (userByPrivateEmail) {
-      sendEmailWithPassword(userByPrivateEmail, email, res);
-    } else {
-      Author.findOne(
-        { divcraftEmail: email },
-        (secErr, userByDivcraftEmail) => {
-          if (secErr) sendError(secErr, res);
-          if (userByDivcraftEmail) {
-            sendEmailWithPassword(userByPrivateEmail, email, res);
-          } else {
-            res.status(401).send('Email is not found.');
-          }
-        }
-      );
-    }
-  });
-});
+router.post('/login', logInUser);
+router.post('/recover-password', recoverPassword);
 
 module.exports = router;
