92dd27bb31ccd45dbfde3c8efd4f9b3775ab9d31
divcraft <t.ogonowski@wp.pl>
2020-12-28 20:11:30 +0100

    add user api to AppHeader component

--- __client/src/components/AppHeader/index.js
+++ __client/src/components/AppHeader/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Cookies from 'js-cookie';
 import { Logo, Wrapper } from 'components';
 import {
   HeaderContainer,
@@ -11,6 +13,22 @@ import {
 } from './style';
 
 const AppHeader = () => {
+  const userId = Cookies.get('user').slice(3).slice(0, -1);
+  const [user, setUser] = useState('');
+  useEffect(() => {
+    Axios.get(`/api/users/${userId}`)
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(res.data);
+          setUser(res.data);
+        } else if (res.status !== 200) {
+          console.log('Something went wrong.');
+        }
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, []);
   return (
     <HeaderContainer>
       <LeftSide>
@@ -20,8 +38,8 @@ const AppHeader = () => {
         <RightSide>
           <NewArticleLink to="/nowy-artykul">Nowy Artykuł</NewArticleLink>
           <UserWindow to="/moje-konto">
-            <UserName>Imię Nazwisko</UserName>
-            <UserPhoto>T</UserPhoto>
+            <UserName>{user.username}</UserName>
+            <UserPhoto>{user && user.username[0].toUpperCase()}</UserPhoto>
           </UserWindow>
         </RightSide>
       </Wrapper>
