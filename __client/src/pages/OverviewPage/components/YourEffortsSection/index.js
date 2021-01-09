c00c058ce05387ac0ae65a31d7580e50f62e1ab2
divcraft <t.ogonowski@wp.pl>
2021-01-09 22:54:36 +0100

    refactoring of YourEffortsSection component

--- __client/src/pages/OverviewPage/components/YourEffortsSection/index.js
+++ __client/src/pages/OverviewPage/components/YourEffortsSection/index.js
@@ -1,62 +1,74 @@
 import React, { useState, useEffect } from 'react';
 import { SectionContainer } from 'components';
 import { useSelector } from 'react-redux';
-import Axios from 'axios';
 import { LineItem, Text, Underline } from './style';
 
-const YouEffortsSection = () => {
-  const [apiData, setApiData] = useState({
-    writtenArticles: null,
-    totalArticleViews: 0,
-    averageRatingOfAllArticles: null,
-    newestPublishedArticle: null,
-    bestRatedArticle: null,
+const YourEffortsSection = () => {
+  const [sectionData, setSectionData] = useState({
+    isCalculated: false,
+    data: {
+      writtenArticles: null,
+      totalArticleViews: 0,
+      averageRatingOfAllArticles: null,
+      newestPublishedArticle: null,
+      bestRatedArticle: null,
+    },
   });
-  const user = useSelector((state) => state.userData.user);
-  useEffect(() => {
-    Axios.get(`/api/articles?userId=${user._id}`).then(({ data }) => {
-      const publishedArticles = data.filter(
-        (article) => article.isPublished === true
-      );
-
-      const writtenArticles = publishedArticles.length;
-
-      const sumOfRatingOfAllArticles =
-        publishedArticles.length > 1
-          ? publishedArticles.reduce(
+  const finishedArticles = useSelector(
+    (state) => state.finishedArticles.articles
+  );
+  const publishedArticles = finishedArticles.filter(
+    (article) => article.isPublished === true
+  );
+  const setWrittenArticles = () => {
+    const writtenArticles = publishedArticles.length;
+    return writtenArticles;
+  };
+  const setAverageRatingOfAllArticles = () => {
+    const sumOfRatingOfAllArticles =
+      publishedArticles.length > 1
+        ? publishedArticles.reduce(
             (acc, { averageRating }) => acc + averageRating,
             0
           )
-          : publishedArticles[0].averageRating;
-      const averageRatingOfAllArticles = (
-        sumOfRatingOfAllArticles / publishedArticles.length
-      ).toFixed(2);
-
-      const newestPublishedArticle = publishedArticles
-        .map((article) => {
-          const transformatedDate = new Date(article.publicationDate);
-          return {
-            ...article,
-            publicationDate: Date.parse(transformatedDate),
-          };
-        })
-        .sort((a, b) => a.publicationDate + b.publicationDate)
-        .map((article) => ({
+        : publishedArticles[0].averageRating;
+    const averageRatingOfAllArticles = (
+      sumOfRatingOfAllArticles / publishedArticles.length
+    ).toFixed(2);
+    return averageRatingOfAllArticles;
+  };
+  const setNewestPublishedArticle = () => {
+    const newestPublishedArticle = publishedArticles
+      .map((article) => {
+        const transformatedDate = new Date(article.publicationDate);
+        return {
           ...article,
-          publicationDate: new Date(article.publicationDate),
-        }))[0];
-
-      const bestRatedArticle = publishedArticles.sort(
-        (a, b) => a.averageRating + b.averageRating
-      )[0];
-
-      setApiData({
-        ...apiData,
-        writtenArticles,
-        averageRatingOfAllArticles,
-        newestPublishedArticle,
-        bestRatedArticle,
-      });
+          publicationDate: Date.parse(transformatedDate),
+        };
+      })
+      .sort((a, b) => a.publicationDate + b.publicationDate)
+      .map((article) => ({
+        ...article,
+        publicationDate: new Date(article.publicationDate),
+      }))[0];
+    return newestPublishedArticle;
+  };
+  const setBestRatedArticle = () => {
+    const bestRatedArticle = publishedArticles.sort(
+      (a, b) => a.averageRating + b.averageRating
+    )[0];
+    return bestRatedArticle;
+  };
+  useEffect(() => {
+    setSectionData({
+      isCalculated: true,
+      data: {
+        ...sectionData,
+        writtenArticles: setWrittenArticles(),
+        averageRatingOfAllArticles: setAverageRatingOfAllArticles(),
+        newestPublishedArticle: setNewestPublishedArticle(),
+        bestRatedArticle: setBestRatedArticle(),
+      },
     });
   }, []);
   const {
@@ -65,58 +77,51 @@ const YouEffortsSection = () => {
     averageRatingOfAllArticles,
     newestPublishedArticle,
     bestRatedArticle,
-  } = apiData;
-  // console.log(apiData);
+  } = sectionData.data;
+  const { isCalculated } = sectionData;
   return (
-    <SectionContainer title="Twoje wyniki">
-      <LineItem>
-        <Text>Napisane artykuły:</Text>
-        <Text>{writtenArticles}</Text>
-      </LineItem>
-      <LineItem>
-        <Text>Łączna ilość odsłon Twoich artykułów:</Text>
-        <Text>{totalArticleViews}</Text>
-        {/* will be added after getting the blog public */}
-      </LineItem>
-      <LineItem>
-        <Text>Średnia ocena za wszystkie artykuły:</Text>
-        <Text>{averageRatingOfAllArticles}</Text>
-      </LineItem>
-      <LineItem>
-        <Text>Najnowszy opublikowany artykuł:</Text>
-        <Underline>
-          {newestPublishedArticle && newestPublishedArticle.header.title}
-        </Underline>
-        <Text>
-          (
-          {newestPublishedArticle &&
-            newestPublishedArticle.publicationDate.getDay()}
-          /
-          {newestPublishedArticle &&
-            (newestPublishedArticle.publicationDate.getMonth() + 1 < 10
+    isCalculated && (
+      <SectionContainer title="Twoje wyniki">
+        <LineItem>
+          <Text>Napisane artykuły:</Text>
+          <Text>{writtenArticles}</Text>
+        </LineItem>
+        <LineItem>
+          <Text>Łączna ilość odsłon Twoich artykułów:</Text>
+          <Text>{totalArticleViews}</Text>
+          {/* will be added after getting the blog public */}
+        </LineItem>
+        <LineItem>
+          <Text>Średnia ocena za wszystkie artykuły:</Text>
+          <Text>{averageRatingOfAllArticles}</Text>
+        </LineItem>
+        <LineItem>
+          <Text>Najnowszy opublikowany artykuł:</Text>
+          <Underline>{newestPublishedArticle.header.title}</Underline>
+          <Text>
+            ({newestPublishedArticle.publicationDate.getDay()}/
+            {newestPublishedArticle.publicationDate.getMonth() + 1 < 10
               ? `0${newestPublishedArticle.publicationDate.getMonth() + 1}`
-              : newestPublishedArticle.publicationDate.getMonth() + 1)}
-          /
-          {newestPublishedArticle &&
-            newestPublishedArticle.publicationDate.getFullYear()}
-          )
-        </Text>
-      </LineItem>
-      <LineItem>
-        <Text>Najlepiej oceniany artykuł:</Text>
-        <Underline>
-          {bestRatedArticle && bestRatedArticle.header.title}
-        </Underline>
-        <Text>({bestRatedArticle && bestRatedArticle.averageRating})</Text>
-      </LineItem>
-      <LineItem>
-        <Text>Najpopularniejszy artykuł:</Text>
-        <Underline>Umieszczenie elementów multimedialnych na stronie</Underline>
-        <Text>(0 odsłon)</Text>
-        {/* will be added after getting the blog public */}
-      </LineItem>
-    </SectionContainer>
+              : newestPublishedArticle.publicationDate.getMonth() + 1}
+            /{newestPublishedArticle.publicationDate.getFullYear()})
+          </Text>
+        </LineItem>
+        <LineItem>
+          <Text>Najlepiej oceniany artykuł:</Text>
+          <Underline>{bestRatedArticle.header.title}</Underline>
+          <Text>({bestRatedArticle.averageRating})</Text>
+        </LineItem>
+        <LineItem>
+          <Text>Najpopularniejszy artykuł:</Text>
+          <Underline>
+            Umieszczenie elementów multimedialnych na stronie
+          </Underline>
+          <Text>(0 odsłon)</Text>
+          {/* will be added after getting the blog public */}
+        </LineItem>
+      </SectionContainer>
+    )
   );
 };
 
-export default YouEffortsSection;
+export default YourEffortsSection;
