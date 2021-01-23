5658454ad9ea3966a0bc9c2dbe9aeb01051e3dfc
divcraft <t.ogonowski@wp.pl>
2021-01-23 22:45:56 +0100

    create notification list logic in NotificationSection component

--- __client/src/pages/OverviewPage/components/NotificationsSection/index.js
+++ __client/src/pages/OverviewPage/components/NotificationsSection/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SectionContainer } from 'components';
+import { useSelector } from 'react-redux';
 import {
   NotificationsList,
   NotificationListItem,
@@ -9,50 +10,61 @@ import {
 } from './style';
 
 const NotificationsSection = () => {
+  const [notificationsLength, setNotificationLength] = useState(5);
+  const notifications = useSelector(
+    (state) => state.notifications.notifications
+  );
+  const finishedArticles = useSelector(
+    (state) => state.finishedArticles.articles
+  );
+  const publishedArticles = finishedArticles.filter(
+    (article) => article.isPublished === true
+  );
+  const notificationsList = notifications
+    .map((notification) => {
+      const articleData = publishedArticles.find(
+        (article) => article._id === notification.article_id
+      );
+      const transformatedDate = new Date(notification.updatedAt);
+      return {
+        ...notification,
+        articleData,
+        updatedAt: Date.parse(transformatedDate),
+      };
+    })
+    .sort((a, b) => a.updatedAt - b.updatedAt)
+    .reverse()
+    .map((item) => {
+      const { notificationType, commentsCounter, articleData, _id } = item;
+      const spanData = () => {
+        if (notificationType === 'newComments') {
+          return `Masz ${commentsCounter} nowych komentarzy pod artykułem`;
+        } else if (notificationType === 'report') {
+          return `Masz nowe zgłoszenie komentarza pod artykułem`;
+        }
+      };
+      return (
+        <NotificationListItem key={_id}>
+          <NotificationText>
+            <span>{spanData()}</span>
+            <ArticleLink>{articleData.header.title}</ArticleLink>
+          </NotificationText>
+          <CheckLink>Sprawdź</CheckLink>
+        </NotificationListItem>
+      );
+    })
+    .splice(0, notificationsLength);
   return (
     <SectionContainer title="Aktualności">
-      <NotificationsList>
-        <NotificationListItem>
-          <NotificationText>
-            <span>Masz 5 nowych komentarzy pod artykułem</span>
-            <ArticleLink>
-              27-letnia Polka z Raciczek Dolnych szczerze po trzech dniach od
-              szczepienia na covid-19
-            </ArticleLink>
-          </NotificationText>
-          <CheckLink>Sprawdź</CheckLink>
-        </NotificationListItem>
-        <NotificationListItem>
-          <NotificationText>
-            <span>Masz 5 nowych komentarzy pod artykułem</span>
-            <ArticleLink>
-              27-letnia Polka z Raciczek Dolnych szczerze po trzech dniach od
-              szczepienia na covid-19
-            </ArticleLink>
-          </NotificationText>
-          <CheckLink>Sprawdź</CheckLink>
-        </NotificationListItem>
-        <NotificationListItem>
-          <NotificationText>
-            <span>Masz 5 nowych komentarzy pod artykułem</span>
-            <ArticleLink>
-              27-letnia Polka z Raciczek Dolnych szczerze po trzech dniach od
-              szczepienia na covid-19
-            </ArticleLink>
-          </NotificationText>
-          <CheckLink>Sprawdź</CheckLink>
-        </NotificationListItem>
-        <NotificationListItem>
-          <NotificationText>
-            <span>Masz 5 nowych komentarzy pod artykułem</span>
-            <ArticleLink>
-              27-letnia Polka z Raciczek Dolnych szczerze po trzech dniach od
-              szczepienia na covid-19
-            </ArticleLink>
-          </NotificationText>
-          <CheckLink>Sprawdź</CheckLink>
-        </NotificationListItem>
-      </NotificationsList>
+      <NotificationsList>{notificationsList}</NotificationsList>
+      {notificationsLength <= notificationsList.length && (
+        <button
+          type="button"
+          onClick={() => setNotificationLength(notificationsLength + 5)}
+        >
+          Załaduj więcej
+        </button>
+      )}
     </SectionContainer>
   );
 };
