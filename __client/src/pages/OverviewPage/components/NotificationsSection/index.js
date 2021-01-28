903bcae9a6a5c3a18adf80d191c76933029f04aa
divcraft <t.ogonowski@wp.pl>
2021-01-28 10:44:45 +0100

    optimize variables in NotificationsSection component with useMemo hook

--- __client/src/pages/OverviewPage/components/NotificationsSection/index.js
+++ __client/src/pages/OverviewPage/components/NotificationsSection/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import {
   LoadMoreButton,
@@ -18,53 +18,59 @@ const NotificationsSection = () => {
   const finishedArticles = useSelector(
     (state) => state.finishedArticles.articles
   );
-  const publishedArticles = finishedArticles.filter(
-    (article) => article.isPublished === true
+  const publishedArticles = useMemo(
+    () => finishedArticles.filter((article) => article.isPublished === true),
+    [finishedArticles]
   );
-  const notificationsList = notifications
-    .map((notification) => {
-      const articleData = publishedArticles.find(
-        (article) => article._id === notification.article_id
-      );
-      const transformatedDate = new Date(notification.updatedAt);
-      return {
-        ...notification,
-        articleData,
-        updatedAt: Date.parse(transformatedDate),
-      };
-    })
-    .sort((a, b) => a.updatedAt - b.updatedAt)
-    .reverse()
-    .map((item) => {
-      const { notificationType, commentsCounter, articleData, _id } = item;
-      const spanData = () => {
-        if (notificationType === 'newComments') {
-          const newCommentsCase = () => {
-            const counterToString = commentsCounter.toString();
-            const lastStringItem = counterToString[counterToString.length - 1];
-            if (counterToString === '1') return 'nowy komentarz';
-            if (lastStringItem >= 2 && lastStringItem <= 4) {
-              return 'nowe komentarze';
-            } else {
-              return 'nowych komentarzy';
+  const notificationsList = useMemo(
+    () =>
+      notifications
+        .map((notification) => {
+          const articleData = publishedArticles.find(
+            (article) => article._id === notification.article_id
+          );
+          const transformatedDate = new Date(notification.updatedAt);
+          return {
+            ...notification,
+            articleData,
+            updatedAt: Date.parse(transformatedDate),
+          };
+        })
+        .sort((a, b) => a.updatedAt - b.updatedAt)
+        .reverse()
+        .map((item) => {
+          const { notificationType, commentsCounter, articleData, _id } = item;
+          const spanData = () => {
+            if (notificationType === 'newComments') {
+              const newCommentsCase = () => {
+                const counterToString = commentsCounter.toString();
+                const lastStringItem =
+                  counterToString[counterToString.length - 1];
+                if (counterToString === '1') return 'nowy komentarz';
+                if (lastStringItem >= 2 && lastStringItem <= 4) {
+                  return 'nowe komentarze';
+                } else {
+                  return 'nowych komentarzy';
+                }
+              };
+              return `Masz ${commentsCounter} ${newCommentsCase()} pod artykułem`;
+            } else if (notificationType === 'report') {
+              return `Masz nowe zgłoszenie komentarza pod artykułem`;
             }
           };
-          return `Masz ${commentsCounter} ${newCommentsCase()} pod artykułem`;
-        } else if (notificationType === 'report') {
-          return `Masz nowe zgłoszenie komentarza pod artykułem`;
-        }
-      };
-      return (
-        <SmallTileListItem key={_id}>
-          <NotificationText>
-            <span>{spanData()}</span>
-            <ArticleLink>{articleData.header.title}</ArticleLink>
-          </NotificationText>
-          <LinkButton pattern="blue">Sprawdź</LinkButton>
-        </SmallTileListItem>
-      );
-    })
-    .splice(0, notificationsLength);
+          return (
+            <SmallTileListItem key={_id}>
+              <NotificationText>
+                <span>{spanData()}</span>
+                <ArticleLink>{articleData.header.title}</ArticleLink>
+              </NotificationText>
+              <LinkButton pattern="blue">Sprawdź</LinkButton>
+            </SmallTileListItem>
+          );
+        })
+        .splice(0, notificationsLength),
+    [notifications, publishedArticles]
+  );
   return (
     <SectionContainer title="Aktualności">
       {notifications.length !== 0 ? (
