e5dc58e24cb8651e50139173d8d1ffccb362ea2d
divcraft <t.ogonowski@wp.pl>
2021-01-31 13:59:52 +0100

    connect publishedArticles api data to MyArticlesPage component

--- __client/src/pages/MyArticlesPage/index.js
+++ __client/src/pages/MyArticlesPage/index.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import { TitleContainer, Wrapper, ListContainer } from 'components';
+import React, { useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  TitleContainer,
+  Wrapper,
+  LoadingIndicator,
+  ListContainer,
+} from 'components';
+import { SUCCESSED } from 'constants';
+import { fetchPublishedArticles, clearPublishedArticles } from 'store/actions';
 import { PublishedArticle } from './components';
 
 const MyArticlesPage = () => {
+  const publishedArticlesLoadingState = useSelector(
+    (state) => state.publishedArticles.loadingState
+  );
+
+  const isDataLoaded = useMemo(
+    () => publishedArticlesLoadingState === SUCCESSED,
+    [publishedArticlesLoadingState]
+  );
+  const dispatch = useDispatch();
+  dispatch(fetchPublishedArticles());
+  useEffect(() => {
+    return () => {
+      dispatch(clearPublishedArticles);
+    };
+  }, []);
   return (
     <>
       <TitleContainer title="Moje artykuły" />
-      <Wrapper>
-        <ListContainer>
-          <PublishedArticle>Artykuł</PublishedArticle>
-          <PublishedArticle>Artykuł</PublishedArticle>
-          <PublishedArticle>Artykuł</PublishedArticle>
-          <PublishedArticle>Artykuł</PublishedArticle>
-          <PublishedArticle>Artykuł</PublishedArticle>
-        </ListContainer>
-      </Wrapper>
+      {isDataLoaded ? (
+        <Wrapper>
+          <ListContainer>
+            <PublishedArticle>Artykuł</PublishedArticle>
+            <PublishedArticle>Artykuł</PublishedArticle>
+            <PublishedArticle>Artykuł</PublishedArticle>
+            <PublishedArticle>Artykuł</PublishedArticle>
+            <PublishedArticle>Artykuł</PublishedArticle>
+          </ListContainer>
+        </Wrapper>
+      ) : (
+        <LoadingIndicator pattern="main" />
+      )}
     </>
   );
 };
