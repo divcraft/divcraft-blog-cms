2bf5a9a9404f2c164d56a1f4629f58e97405662a
divcraft <t.ogonowski@wp.pl>
2021-02-04 19:16:31 +0100

    sort articles in PublishedArticlesList component

--- __client/src/pages/MyArticlesPage/components/PublishedArticleList/index.js
+++ __client/src/pages/MyArticlesPage/components/PublishedArticleList/index.js
@@ -8,6 +8,15 @@ const PublishedArticleList = () => {
     (state) => state.publishedArticles.articles
   );
   const publishedArticlesList = publishedArticles
+    .map((article) => {
+      const transformatedDate = new Date(article.publicationDate);
+      return {
+        ...article,
+        publicationDate: Date.parse(transformatedDate),
+      };
+    })
+    .sort((a, b) => a.publicationDate - b.publicationDate)
+    .reverse()
     .map((article) => ({
       ...article,
       publicationDate: new Date(article.publicationDate),
