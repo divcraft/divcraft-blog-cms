1610496df0be5beff22dafe121eeffb491886282
divcraft <t.ogonowski@wp.pl>
2021-02-10 19:40:01 +0100

    create EditStagePage compoment template

--- __client/src/pages/WaitingRoomPage/pages/EditStage/index.js
+++ __client/src/pages/WaitingRoomPage/pages/EditStage/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import { Wrapper } from 'components';
+import React, { useEffect, useMemo } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+import { Wrapper, LoadingIndicator } from 'components';
+// import { SUCCESSED } from 'constants';
+// import {
+//   fetchPublishedArticles,
+//   clearPublishedArticles,
+//   fetchComments,
+//   clearComments,
+// } from 'store/actions';
+// import { ToGettingPublicArticleList } from './components';
 
-const WaitingRoomPage = () => {
+const EditStagePage = () => {
+  // const publishedArticlesLoadingState = useSelector(
+  //   (state) => state.publishedArticles.loadingState
+  // );
+  // const commentsLoadingState = useSelector(
+  //   (state) => state.comments.loadingState
+  // );
+  // const isDataLoaded = useMemo(
+  //   () =>
+  //     publishedArticlesLoadingState === SUCCESSED &&
+  //     commentsLoadingState === SUCCESSED,
+  //   [publishedArticlesLoadingState, commentsLoadingState]
+  // );
+  const isDataLoaded = useMemo(() => true, []);
+  // const dispatch = useDispatch();
+  // dispatch(fetchPublishedArticles());
+  // dispatch(fetchComments());
+  useEffect(() => {
+    return () => {
+      // dispatch(clearPublishedArticles);
+      // dispatch(clearComments);
+    };
+  }, []);
   return (
     <>
-      <Wrapper>
-        <div>Tryb Edycji</div>
-      </Wrapper>
+      {isDataLoaded ? (
+        <Wrapper>
+          {/* <PublishedArticleList /> */}
+          <div>edit stage</div>
+        </Wrapper>
+      ) : (
+        <LoadingIndicator pattern="main" />
+      )}
     </>
   );
 };
 
-export default WaitingRoomPage;
+export default EditStagePage;
