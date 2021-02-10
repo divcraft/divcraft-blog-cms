809e13e7aaf39bdd5be3b9f729fb2e99d1cec187
divcraft <t.ogonowski@wp.pl>
2021-02-10 20:33:57 +0100

    connect unpublished articles data to pages at WaitingRoomPage component

--- __client/src/pages/WaitingRoomPage/pages/EditStage/index.js
+++ __client/src/pages/WaitingRoomPage/pages/EditStage/index.js
@@ -1,43 +1,29 @@
 import React, { useEffect, useMemo } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Wrapper, LoadingIndicator } from 'components';
-// import { SUCCESSED } from 'constants';
-// import {
-//   fetchPublishedArticles,
-//   clearPublishedArticles,
-//   fetchComments,
-//   clearComments,
-// } from 'store/actions';
+import { SUCCESSED } from 'constants';
+import { fetchToEditArticles, clearToEditArticles } from 'store/actions';
 // import { ToGettingPublicArticleList } from './components';
 
 const EditStagePage = () => {
-  // const publishedArticlesLoadingState = useSelector(
-  //   (state) => state.publishedArticles.loadingState
-  // );
-  // const commentsLoadingState = useSelector(
-  //   (state) => state.comments.loadingState
-  // );
-  // const isDataLoaded = useMemo(
-  //   () =>
-  //     publishedArticlesLoadingState === SUCCESSED &&
-  //     commentsLoadingState === SUCCESSED,
-  //   [publishedArticlesLoadingState, commentsLoadingState]
-  // );
-  const isDataLoaded = useMemo(() => true, []);
-  // const dispatch = useDispatch();
-  // dispatch(fetchPublishedArticles());
-  // dispatch(fetchComments());
+  const toEditArticlesLoadingState = useSelector(
+    (state) => state.toEditArticles.loadingState
+  );
+  const isDataLoaded = useMemo(() => toEditArticlesLoadingState === SUCCESSED, [
+    toEditArticlesLoadingState,
+  ]);
+  const dispatch = useDispatch();
+  dispatch(fetchToEditArticles());
   useEffect(() => {
     return () => {
-      // dispatch(clearPublishedArticles);
-      // dispatch(clearComments);
+      dispatch(clearToEditArticles);
     };
   }, []);
   return (
     <>
       {isDataLoaded ? (
         <Wrapper>
-          {/* <PublishedArticleList /> */}
+          {/* <ToEditArticleList /> */}
           <div>edit stage</div>
         </Wrapper>
       ) : (
--- __client/src/pages/WaitingRoomPage/pages/ToGettingPublic/index.js
+++ __client/src/pages/WaitingRoomPage/pages/ToGettingPublic/index.js
@@ -1,36 +1,26 @@
 import React, { useEffect, useMemo } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Wrapper, LoadingIndicator } from 'components';
-// import { SUCCESSED } from 'constants';
-// import {
-//   fetchPublishedArticles,
-//   clearPublishedArticles,
-//   fetchComments,
-//   clearComments,
-// } from 'store/actions';
+import { SUCCESSED } from 'constants';
+import {
+  fetchUnpublishedArticles,
+  clearUnpublishedArticles,
+} from 'store/actions';
 // import { ToGettingPublicArticleList } from './components';
 
 const ToGettingPublicPage = () => {
-  // const publishedArticlesLoadingState = useSelector(
-  //   (state) => state.publishedArticles.loadingState
-  // );
-  // const commentsLoadingState = useSelector(
-  //   (state) => state.comments.loadingState
-  // );
-  // const isDataLoaded = useMemo(
-  //   () =>
-  //     publishedArticlesLoadingState === SUCCESSED &&
-  //     commentsLoadingState === SUCCESSED,
-  //   [publishedArticlesLoadingState, commentsLoadingState]
-  // );
-  const isDataLoaded = useMemo(() => true, []);
-  // const dispatch = useDispatch();
-  // dispatch(fetchPublishedArticles());
-  // dispatch(fetchComments());
+  const unpublishedArticlesLoadingState = useSelector(
+    (state) => state.unpublishedArticles.loadingState
+  );
+  const isDataLoaded = useMemo(
+    () => unpublishedArticlesLoadingState === SUCCESSED,
+    [unpublishedArticlesLoadingState]
+  );
+  const dispatch = useDispatch();
+  dispatch(fetchUnpublishedArticles());
   useEffect(() => {
     return () => {
-      // dispatch(clearPublishedArticles);
-      // dispatch(clearComments);
+      dispatch(clearUnpublishedArticles);
     };
   }, []);
   return (
