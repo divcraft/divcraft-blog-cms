b70f48b783d5be4459e4b4a02394ee448766ef32
divcraft <t.ogonowski@wp.pl>
2021-02-18 12:05:08 +0100

    add markArticle button in UnpublishedArticle component

--- __client/src/pages/WaitingRoomPage/components/UnpublishedArticle/index.js
+++ __client/src/pages/WaitingRoomPage/components/UnpublishedArticle/index.js
@@ -17,15 +17,25 @@ import {
 
 const UnpublishedArticle = ({ article }) => {
   const categories = useSelector((state) => state.categories.categories);
+  const markedArticles = useSelector(
+    (state) => state.userData.user.markedArticles
+  );
   const {
+    _id,
     header: { title },
     category_id,
     updatedAt,
   } = article;
+  const isMarked =
+    markedArticles.find((articleId) => articleId === _id) || false;
+  console.log({ isMarked });
   const articleCategory = useMemo(
     () => categories.find((category) => category._id === category_id),
     [categories]
   );
+  const handleMarkArticle = () => {
+    console.log({ isMarked });
+  };
   return (
     <TileListItem pattern="big">
       <Image src="" alt="" />
@@ -36,7 +46,9 @@ const UnpublishedArticle = ({ article }) => {
           </div>
           <DataContainer>
             <span>
-              <StyledFontAwesome icon={faStar} />
+              <button type="button" onClick={() => handleMarkArticle()}>
+                <StyledFontAwesome icon={faStar} />
+              </button>
             </span>
             <span>
               <SemiboldText>Ostatnio zmodyfikowano: </SemiboldText>
