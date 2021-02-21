65720eebda12089b57aec56dfd699b48a5e51770
divcraft <t.ogonowski@wp.pl>
2021-02-21 19:48:34 +0100

    create userData actions for editing marked articles list

--- __client/src/store/actions/userData.actions.js
+++ __client/src/store/actions/userData.actions.js
@@ -4,7 +4,14 @@ import {
   FETCH_USER_DATA_PROMISE,
   FETCH_USER_DATA_SUCCESS,
   FETCH_USER_DATA_FAIL,
+  ADD_MARKED_ARTICLE_PROMISE,
+  ADD_MARKED_ARTICLE_SUCCESS,
+  ADD_MARKED_ARTICLE_FAIL,
+  REMOVE_MARKED_ARTICLE_PROMISE,
+  REMOVE_MARKED_ARTICLE_SUCCESS,
+  REMOVE_MARKED_ARTICLE_FAIL,
 } from 'constants';
+import { useSelector } from 'react-redux';
 
 export const fetchUserData = (id) => (dispatch) => {
   useEffect(() => {
@@ -25,4 +32,64 @@ export const fetchUserData = (id) => (dispatch) => {
         throw err;
       });
   }, []);
+};
+
+export const addMarkedArticle = (articleId) => (dispatch) => {
+  const userData = useSelector((state) => state.userData);
+  const newMarkedArticles = [...userData.markedArticles, articleId];
+  const user = {
+    ...userData,
+    markedArticles: newMarkedArticles,
+  };
+  // useEffect(() => {
+  dispatch({
+    type: ADD_MARKED_ARTICLE_PROMISE,
+  });
+  Axios.put(`/api/users/${userData._id}`, {
+    user,
+  })
+    .then((res) => {
+      dispatch({
+        type: ADD_MARKED_ARTICLE_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: ADD_MARKED_ARTICLE_FAIL,
+      });
+      throw err;
+    });
+  // }, []);
+};
+
+export const removeMarkedArticle = (articleId) => (dispatch) => {
+  const userData = useSelector((state) => state.userData);
+  const newMarkedArticles = userData.markedArticles.filter(
+    (article) => article !== articleId
+  );
+  const user = {
+    ...userData,
+    markedArticles: newMarkedArticles,
+  };
+  // useEffect(() => {
+  dispatch({
+    type: REMOVE_MARKED_ARTICLE_PROMISE,
+  });
+  Axios.put(`/api/users/${userData._id}`, {
+    user,
+  })
+    .then((res) => {
+      dispatch({
+        type: REMOVE_MARKED_ARTICLE_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: REMOVE_MARKED_ARTICLE_FAIL,
+      });
+      throw err;
+    });
+  // }, []);
 };
