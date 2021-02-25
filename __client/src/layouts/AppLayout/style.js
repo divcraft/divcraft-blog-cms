19f992a5faced4d97097133ecb7da83e19c5daa5
divcraft <t.ogonowski@wp.pl>
2021-02-25 18:33:30 +0100

    fix styles in AppLayout component

--- __client/src/layouts/AppLayout/style.js
+++ __client/src/layouts/AppLayout/style.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const LayoutContainer = styled.main`
   grid-area: layout;
-  & > div:nth-child(2) {
+  & > div:nth-child(2),
+  & > div:nth-child(3) {
     height: calc(100vh - 150px);
     overflow-y: scroll;
     position: relative;
