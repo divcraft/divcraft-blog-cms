6b74320c7036af7654c5a042ef0c4e35194e3ae0
divcraft <t.ogonowski@wp.pl>
2021-03-06 23:49:49 +0100

    create SectionEditor component

---
+++ __client/src/pages/NewArticlePage/components/SectionEditor/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './style';
+
+const SectionEditor = () => {
+  return <Container>section editor</Container>;
+};
+
+export default SectionEditor;
